14a1186c0c19257b5522bb93135d700f072e4e7b
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2024-04-19 20:00:16 +0100

    Added eslint and semantic-release
    Signed-off-by: Joshua Baker <160484+joshuabaker@users.noreply.github.com>

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
