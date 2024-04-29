56bbb8ccbce4e5b0a3c7492a521482b03dfb4edb
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2024-04-29 12:14:40 +0100

    Adjust releases
    Signed-off-by: Joshua Baker <160484+joshuabaker@users.noreply.github.com>

--- release.config.js
+++ release.config.js
@@ -3,13 +3,6 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer", // analyzes commits to determine the semantic version bump
     "@semantic-release/release-notes-generator", // generates release notes from commit messages
-    "@semantic-release/changelog", // updates the CHANGELOG.md file
     "@semantic-release/npm", // publishes the package to npm
-    "@semantic-release/git", // commits changes to the package.json file and CHANGELOG.md
-    {
-      assets: ["package.json", "CHANGELOG.md"],
-      message:
-        "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-    },
   ],
 };
